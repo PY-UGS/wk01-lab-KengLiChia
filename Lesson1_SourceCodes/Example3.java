aee2aed0ea869aae274c44cd59370937d64d64de
KengLiChia <92797060+KengLiChia@users.noreply.github.com>
2022-01-11 14:03:04 +0800

    Add files via upload

---
+++ Lesson1_SourceCodes/Example3.java
@@ -0,0 +1,10 @@
+public class Example3 {
+    public static void main(String[] args) {
+        System.out.println("Value of i: ");
+        for (int i = 102; i > 66; i--) {
+            if (i%2 !=0){
+                System.out.print(i + ",");
+            }
+        }
+    }
+}
